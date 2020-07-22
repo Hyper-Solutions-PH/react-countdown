ce230338a18ae77bc4b5264deb5639dcdbf3fa85
renee.llup <renee.llup@gmail.com>
2020-07-22 18:26:58 +0800

    Customize for Kabaw Technologies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
-import Countdown from './Countdown.js';
-import logo from './logo.svg';
-import github from './github.png';
+import React, { Component } from "react";
+import Countdown from "./Countdown.js";
+import logo from "./logo.svg";
+// import github from "./github.png";
 
 class App extends Component {
   render() {
-    const currentDate = new Date();
-    const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
+    // const currentDate = new Date();
+    // const year =
+    //   currentDate.getMonth() === 11 && currentDate.getDate() > 23
+    //     ? currentDate.getFullYear() + 1
+    //     : currentDate.getFullYear();
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Countdown</h2>
+          {/* <h2>React Countdown</h2> */}
 
-          <a href="https://github.com/kristinbaumann/react-countdown" target="_blank">
+          {/* <a
+            href="https://github.com/kristinbaumann/react-countdown"
+            target="_blank"
+          >
             <img src={github} alt="github" className="githubIcon" />
             <span>View on Github</span>
-          </a>
+          </a> */}
         </div>
 
-        <h3 className="title">Christmas Eve is coming soon (Midnight of 23rd to 24th Dec, UTC time):</h3>
-        <Countdown date={`${year}-12-24T00:00:00`} />
+        <h3 className="title">
+          A cost effective systems coming soon on the 9nth of November 2020 UTC
+          time):
+        </h3>
+        <Countdown date={`2020-11-09T00:00:00`} />
       </div>
     );
   }
